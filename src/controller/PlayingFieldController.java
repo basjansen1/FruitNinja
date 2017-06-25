2da92050a203f0142760419477743599b6dc9a7f
Bas Jansen <bjansen9@avans.nl>
2017-06-25 12:56:37 +0200

    added constant spawning of fruit, and fixed a bug where the fruit would spawn in the middle of the screen

--- src/controller/PlayingFieldController.java
+++ src/controller/PlayingFieldController.java
@@ -53,67 +53,71 @@ public class PlayingFieldController extends MouseAdapter {
 	private class Animate implements Runnable {
 		@Override
 		public void run() {
-			GameObjectType gameObjectType = GameObjectType.getRandomFruitType();
-			SpawnSide spawnSide = SpawnSide.getRandomSide();
-			
-			if (gameObjectType == GameObjectType.BOMB) {
-				gameObject = new Bomb();
-			} else {
-				gameObject = new Fruit();
-			}
-			
-			new SlashTrailSectionController(slash, gameObject);
-			
-			Random r = new Random();
-			
-			if (spawnSide == SpawnSide.BOTTOM || spawnSide == SpawnSide.TOP) {
-				x = r.nextInt(470);
-			} else {
-				y = r.nextInt(470);
-			}
-			
-			boolean firstTime = true;
-			
-			for(;;) {
-				switch(spawnSide) {
-					case BOTTOM:
-						if (firstTime) {
-							y = 500;
-							firstTime = false;
-						}
-						y--;
-						break;
-					case TOP:	
-						y++;
-						break;
-					case LEFT:
-						x++;
-						break;
-					case RIGHT:
-						if (firstTime) {
-							x = 500;
-							firstTime = false;
-						}
-						x--;
-						break;
-				}
-
-				// Repaint the screen so the GameObject gets updated/animated
-				gameObject.setX(x);
-				gameObject.setY(y);
-				gameObject.setObjectType(gameObjectType);
+			for (;;) {
+				GameObjectType gameObjectType = GameObjectType.getRandomFruitType();
+				SpawnSide spawnSide = SpawnSide.getRandomSide();
 				
-				gameView.animateGameObject(gameObject);
-				
-				// -80 So you know for sure the fruit or bomb is out of the screen.
-				if (x > 500 || x < -80 
-						|| y > 500 || y < -80) {
-					break;
+				if (gameObjectType == GameObjectType.BOMB) {
+					gameObject = new Bomb();
+				} else {
+					gameObject = new Fruit();
 				}
 				
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
+				new SlashTrailSectionController(slash, gameObject);
+				
+				Random r = new Random();
+				
+				if (spawnSide == SpawnSide.BOTTOM || spawnSide == SpawnSide.TOP) {
+					x = r.nextInt(470);
+				} else {
+					y = r.nextInt(470);
+				}
+				
+				boolean firstTime = true;
+				
+				for(;;) {
+					switch(spawnSide) {
+						case BOTTOM:
+							if (firstTime) {
+								y = 500;
+								firstTime = false;
+							}
+							y--;
+							break;
+						case TOP:	
+							y++;
+							break;
+						case LEFT:
+							x++;
+							break;
+						case RIGHT:
+							if (firstTime) {
+								x = 500;
+								firstTime = false;
+							}
+							x--;
+							break;
+					}
+	
+					// Repaint the screen so the GameObject gets updated/animated
+					gameObject.setX(x);
+					gameObject.setY(y);
+					gameObject.setObjectType(gameObjectType);
+					
+					gameView.animateGameObject(gameObject);
+					
+					// -80 So you know for sure the fruit or bomb is out of the screen.
+					if (x > 500 || x < -80 
+							|| y > 500 || y < -80) {
+						x = 0;
+						y = 0;
+						break;
+					}
+					
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+					}
 				}
 			}
 		}
