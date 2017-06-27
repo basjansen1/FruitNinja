5b53ec69bedc4cc35b93b582c88dcfd94bb490e8
Bas Jansen <bjansen9@avans.nl>
2017-06-27 13:31:03 +0200

    code cleaning

--- src/model/Fruit.java
+++ src/model/Fruit.java
@@ -8,11 +8,7 @@ public class Fruit extends GameObject {
 	private int score;
 
 	public int getScore() {
-		if (super.getSize() == 30) {
-			return score;
-		} else { 
-			return score;
-		}
+		return score;
 	}
 
 	public void setScore(int score) {
